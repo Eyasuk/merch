d2967f60cb633f55795ad207445c54edec7c6405
Eyasuk <eyasukibru290@gmail.com>
2023-09-05 02:17:37 +0300

    signup layout

--- apps/web/components/features/signup/index.tsx
+++ apps/web/components/features/signup/index.tsx
@@ -1,37 +1,51 @@
 'use client';
-import { Button, Divider, Layout, Typography } from 'antd';
+import { Button, Divider, Typography } from 'antd';
+import { useState } from 'react';
 
 import styles from './signup.module.scss';
 
 const { Title, Text } = Typography;
 
 export default function SignUp() {
+  const [openForm, setOpenForm] = useState<
+    'choose' | 'withEmail' | 'withPhone'
+  >('choose');
+
   return (
     <div className={styles.signupBody}>
       <div className={styles.advert}>s</div>
+
       <div className={styles.signupMethods}>
-        <Title level={3}>Sign up to Libes</Title>
-        <Button className={styles.button} size="large">
-          Continue with Email{' '}
-        </Button>
-        <Button className={styles.button} size="large">
-          Continue with Phone{' '}
-        </Button>
-        <div className={styles.divider}>
-          <Divider />
-        </div>
-        <Button className={styles.button} type="primary" size="large">
-          Signup with Google{' '}
-        </Button>
-        <div className={styles.bottom}>
-          <Text type="secondary">
-            By creating an account you agree with our Terms of Service and
-            Privacy Policy
-          </Text>
-          <Text>
-            Already have an account? <a> Sign in</a>
-          </Text>
-        </div>
+        {openForm == 'choose' ? (
+          <>
+            <Title level={3}>Sign up to Libes</Title>
+            <Button className={styles.button} size="large">
+              Continue with Email{' '}
+            </Button>
+            <Button className={styles.button} size="large">
+              Continue with Phone{' '}
+            </Button>
+            <div className={styles.divider}>
+              <Divider />
+            </div>
+            <Button className={styles.button} type="primary" size="large">
+              Signup with Google{' '}
+            </Button>
+            <div className={styles.bottom}>
+              <Text type="secondary">
+                By creating an account you agree with our Terms of Service and
+                Privacy Policy
+              </Text>
+              <Text>
+                Already have an account? <a> Sign in</a>
+              </Text>
+            </div>
+          </>
+        ) : openForm == 'withEmail' ? (
+          <div></div>
+        ) : (
+          <div> </div>
+        )}
       </div>
     </div>
   );
--- apps/web/theme/themeConfig.ts
+++ apps/web/theme/themeConfig.ts
@@ -4,6 +4,7 @@ const theme: ThemeConfig = {
   token: {
     fontSize: 16,
     colorPrimary: '#22A39F',
+    fontFamily: 'Helvetica Neue',
   },
   algorithm: antdTheme.darkAlgorithm,
   //algorithm: antdTheme.defaultAlgorithm,
