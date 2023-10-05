57aaf2435927688a842da6e02927c2340d77f49e
Eyasuk <eyasukibru290@gmail.com>
2023-10-05 17:47:13 +0300

    advert ui in home page

--- apps/web/components/modules/advert/index.tsx
+++ apps/web/components/modules/advert/index.tsx
@@ -34,7 +34,7 @@ export default function AdvertBox(): JSX.Element {
           setImageIndex((prevIndex) => (prevIndex + 1) % currentImage.length);
         }
         setZoomed(false);
-      }, 300); // Duration of the zoom animation in milliseconds
+      }, 3000); // Duration of the zoom animation in milliseconds
     }, 10000);
     return () => {
       clearInterval(timer);
